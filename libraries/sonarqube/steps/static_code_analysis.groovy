ed1456bd54246e45dc426a411d35815eaede6ad4
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 11:56:06 +0900

    Update static_code_analysis.groovy

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,10 +1,31 @@
 void call(){
     stage("SonarQube: Static Code Analysis"){
         println "static code analysis from the sonarqube library"
+//         node {
+//             def scannerHome = tool 'SonarScanner 4.0’;
+//             withSonarQubeEnv('My SonarQube Server') { 
+//                 sh "${scannerHome}/bin/sonar-scanner"
+//             }
+//         }
+        // parse configuration 
+        String scannerVersion = config.scanner_version ?: "SonarScanner 4.0"
+        String serverName = config.server_name ?: "My SonarQube Server"
+        Boolean enforceQualityGate = config.containsKey("enforce_quality_gate") ?
+                config.enforce_quality_gate : true
         node {
-            def scannerHome = tool 'SonarScanner 4.0’;
-            withSonarQubeEnv('My SonarQube Server') { 
+            def scannerHome = tool(scannerVersion)
+            withSonarQubeEnv(serverName) { 
                 sh "${scannerHome}/bin/sonar-scanner"
+            }
+        }
+        timeout(time: 1, unit: 'HOURS') {
+            def qg = waitForQualityGate() 
+            if (qg.status != 'OK') {
+                if(enforceQualityGate){
+                    error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                } else {
+                    warning "Quality gate failure: ${qg.status}"
+                }
             }
         }
     }
