c093a696a1d0248fdb86f8abf932f759b77f2e6b
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 12:43:53 +0900

    Update build.groovy

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -2,7 +2,8 @@ void call(){
     stage("Gradle: Build"){ 
         println "build from the gradle library" 
         docker.image("gradle").inside{
-            sh "git clone https://github.com/pythonkazuma/Jenkins_Practical_Guide_3rd_Edition.git"
+            //sh "git clone https://github.com/pythonkazuma/Jenkins_Practical_Guide_3rd_Edition.git"
+            sh "cd https://github.com/pythonkazuma/Jenkins_Practical_Guide_3rd_Edition"
             sh "gradle clean build"
         }
     }
