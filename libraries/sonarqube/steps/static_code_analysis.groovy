07f85acae4d221a1b4c3d007ebb62e4762bb4e9d
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 11:31:13 +0900

    Update static_code_analysis.groovy

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,5 +1,11 @@
 void call(){
     stage("SonarQube: Static Code Analysis"){
         println "static code analysis from the sonarqube library"
+        node {
+            def scannerHome = tool 'SonarScanner 4.0’;
+            withSonarQubeEnv('My SonarQube Server') { 
+                sh "${scannerHome}/bin/sonar-scanner"
+            }
+        }
     }
 }
