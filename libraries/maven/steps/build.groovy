82662a09ee0fbb3cc4599c2597c3acf1e069ea3c
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 13:49:27 +0900

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,5 +1,8 @@
 void call(){
     stage("Maven: Build"){
         println "build from the maven library"
+        docker.image("maven").inside{
+            sh "mvn clean package"
+        }
     }
 }
