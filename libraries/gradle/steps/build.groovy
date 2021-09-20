a7cfd1f9b6492f7e55474d93510ed1da6582b93c
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 11:14:30 +0900

    Update build.groovy

--- libraries/gradle/steps/build.groovy
+++ libraries/gradle/steps/build.groovy
@@ -1,5 +1,8 @@
 void call(){
     stage("Gradle: Build"){ 
         println "build from the gradle library" 
+        docker.image(”gradle").inside{
+            sh ”gradle clean build"
+        }
     }
 }
